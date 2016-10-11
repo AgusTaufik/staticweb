5a5edb0cd28304a3e133aefbb5ae88ef4141128d
AgusTaufik <lem.fox120@gmail.com>
2016-10-11 11:23:38 +0700

    First commit

---
+++ web3.go
@@ -0,0 +1,17 @@
+import (
+"fmt"
+"log"
+"net/http"
+)
+func messageHandler(w http.ResponseWriter, r *http.Request) {
+fmt.Fprintf(w, "Welcome to Go Web Development")
+}
+}
+func main() {
+mux := http.NewServeMux()
+// Convert the messageHandler function to a HandlerFunc type
+mh := http.HandlerFunc(messageHandler)
+mux.Handle("/welcome", mh)
+log.Println("Listening...")
+http.ListenAndServe(":1908", mux)
+}
